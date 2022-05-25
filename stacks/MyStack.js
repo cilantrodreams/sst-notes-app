0b736161cbdc48e6deec9e30ea68b22da1a4cb3f
Thomas Ngo <thomasngo818@gmail.com>
2022-05-25 16:02:15 -0700

    print endpoint url in output

--- stacks/MyStack.js
+++ stacks/MyStack.js
@@ -1,9 +1,14 @@
 import { Api } from "@serverless-stack/resources";
 
 export function MyStack({ stack }) {
-  new Api(stack, "api", {
+  const api = new Api(stack, "api", {
     routes: {
       "GET /": "functions/lambda.handler",
     },
+  });
+
+  // show the endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
   });
 }
